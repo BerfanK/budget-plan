df24e1933d4f0dfaec416e107463238c051943a5
BerfanK <berfan.korkmaz@stud.edubs.ch>
2021-10-02 10:41:43 +0200

    first commit

---
+++ imports/navbar.php
@@ -0,0 +1,54 @@
+<?php
+
+$path = substr($_SERVER['SCRIPT_NAME'], 1);
+$file = basename($path, ".php");
+
+$loggedIn = false;
+if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true) $loggedIn = true;
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item" style="margin: 0 !important">
+                    <a class="nav-link ps-n5 <?php if ($file == 'index') echo 'current'; ?>" href="./">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php if ($file == 'projects' || $file == 'project') echo 'current'; ?>" href="./plan">Plan ändern</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <?php 
+
+                $currentLogin = "current";
+                if ($file !== 'login') $currentLogin = "";
+                if (!$loggedIn) {
+                    echo 
+                    '
+                    <li class="nav-item">
+                        <a class="nav-link ' . $currentLogin . '" href="./login">Anmelden</a>
+                    </li>
+                    ';
+                } else {
+                    echo 
+                    '
+                    <li class="nav-item">
+                        <a class="nav-link" href="./logout">Abmelden</a>
+                    </li>
+                    ';
+                }
+
+                ?>
+                <li class="nav-item">
+                    <a href="" class="nav-link"><i class="far fa-user-circle"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+ 
+    <?php include_once "imports/head.php"; ?>
+
+    <body>
+        <?php include_once "imports/navbar.php"; ?>
+
+        
+
+        <?php include_once "imports/scripts.php"; ?>
+    </body>
+
+</html>
