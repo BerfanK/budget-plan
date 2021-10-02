8ae9b1f7ba57e8bc5abd786c68e007dcc40185a0
BerfanK <berfan.korkmaz@stud.edubs.ch>
2021-10-02 10:43:01 +0200

    Add Index cards

--- index.php
+++ index.php
@@ -6,7 +6,58 @@
     <body>
         <?php include_once "imports/navbar.php"; ?>
 
-        
+        <div class="container my-5">
+
+            <div class="title">Ihr Budgetplan</div>
+            <div class="title-text">Hier haben Sie einen Einblick über Ihre Ein- und Ausgaben.</div>
+
+            <hr class="mt-3 mb-5">
+
+            <!-- Summary Card -->
+            <div class="card-title">Zusammenfassung</div>
+            <div class="card mb-5 rounded-0">
+                <div class="card-body">
+
+                    <div>
+                        <span class="card-input">Einkommen</span>
+                        <span class="card-value">$ 40,000</span>
+                    </div>
+
+                    <div>
+                        <span class="card-input">Ausgaben</span>
+                        <span class="card-value">$ 43,000</span>
+                    </div>
+
+                    <hr>
+
+                    <div>
+                        <span class="card-input text-danger">Verlust</span>
+                        <span class="card-value">$  3,000</span>
+                    </div>
+
+                </div>
+            </div>
+            <!-- End Summary Card -->
+
+            <!-- Income Card -->
+            <div class="card-title">Einkommen</div>
+            <div class="card mb-5 rounded-0">
+                <div class="card-body">
+
+                </div>
+            </div>
+            <!-- End Income Card -->
+
+            <!-- Outcome Card -->
+            <div class="card-title">Ausgaben</div>
+            <div class="card mb-5 rounded-0">
+                <div class="card-body">
+
+                </div>
+            </div>
+            <!-- End Outcome Card -->
+
+        </div>
 
         <?php include_once "imports/scripts.php"; ?>
     </body>
